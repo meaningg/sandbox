e51ec17ce6da83aae21c499f8d80ae1982cad6be
Mean <thomassred@gmail.com>
2021-09-20 19:42:24 +0300

    v0.22
    -ChatApp auth added

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -10,6 +10,20 @@ import {
   faTrash,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import firebase from "firebase/compat/app";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+function Auth() {
+  const [user] = useAuthState(auth);
+  return user ? <Chat /> : <AuthBox />;
+}
+
 function Chat() {
   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -20,7 +34,7 @@ function Chat() {
       <Fade duration={300} delay={getRandomInt(700)}>
         <div className="chatsList">
           <div className="chatsList__item chatsList__item_choosen">
-            <span>HornyChat</span>
+            <span>HornyChat active</span>
             <FontAwesomeIcon
               className="chatsList__item_deleteIcon"
               icon={faTrash}
@@ -56,26 +70,43 @@ function Chat() {
         <div className="sideBar">
           <div className="sideBar__button">
             <span>Create a chat room</span>
-            <FontAwesomeIcon icon={faPlus} />
+            <FontAwesomeIcon className="sideBar__icon" icon={faPlus} />
           </div>
           <div className="sideBar__button">
             <span>Enter the chat room</span>
-            <FontAwesomeIcon icon={faSignInAlt} />
+            <FontAwesomeIcon className="sideBar__icon" icon={faSignInAlt} />
           </div>
           <div className="sideBar__button">
             <span>Settings</span>
-            <FontAwesomeIcon icon={faCog} />
+            <FontAwesomeIcon className="sideBar__icon" icon={faCog} />
           </div>
-          <Link to="/">
-            <div className="sideBar__button">
+
+          <div onClick={() => auth.signOut()} className="sideBar__button">
+            <Link to="/">
               <span>Exit</span>
-              <FontAwesomeIcon icon={faPowerOff} />
-            </div>
-          </Link>
+              <FontAwesomeIcon className="sideBar__icon" icon={faPowerOff} />
+            </Link>
+          </div>
         </div>
       </Fade>
     </div>
   );
 }
 
-export default Chat;
+function AuthBox() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+  return (
+    <div className="noselect authBox">
+      <span> To use the chat room, you need to log in via</span>{" "}
+      <h4 onClick={signInWithGoogle}>
+        <FontAwesomeIcon icon={faGoogle} />
+        oogle
+      </h4>
+    </div>
+  );
+}
+
+export default Auth;
